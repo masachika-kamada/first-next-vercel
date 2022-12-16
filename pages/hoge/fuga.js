725acaac987d4770d2e724483970e58fb0fc9f99
masachika <masachika.kmd@gmail.com>
2022-12-17 01:58:48 +0900

    レイアウト変更

---
+++ pages/hoge/fuga.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return <h1>Next.jsのVercelへのデプロイ完了！</h1>;
+}
